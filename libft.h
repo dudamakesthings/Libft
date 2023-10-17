c1c34f18fc1f6fea857930e23bf6078d99e356cd
Eduarda R <145663997+dudamakesthings@users.noreply.github.com>
2023-10-17 16:04:56 +0200

    Header with standard libraries

---
+++ libft.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   libft.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: edribeir <edribeir@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/10/17 13:59:00 by edribeir      #+#    #+#                 */
+/*   Updated: 2023/10/17 15:58:13 by edribeir      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H //prevent to the compiler uses the header twice
+
+// standard functions
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stddef.h> // size_t lib > unsigned  integer  type capable of storing values in the range [0, SIZE_MAX].
+#include <bsd/string.h> // functions from BSD systems -lbsd
+
+#endif
