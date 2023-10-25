28161358a0e20b8fc60e2c127a03046b1c1c693d
dudamakesthings <dudamakesthings@gmail.com>
2023-10-25 14:34:52 +0200

    ajust norminnete

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,47 +6,38 @@
 /*   By: edribeir <edribeir@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/19 11:39:26 by edribeir      #+#    #+#                 */
-/*   Updated: 2023/10/19 16:25:07 by edribeir      ########   odam.nl         */
+/*   Updated: 2023/10/25 13:55:40 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-unsigned int ft_strlen (const char *str)
-{   
-    unsigned int i;
-    i = 0;
-    while(str[i] != '\0')
-        i++;
-    return i;
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*newstr;
+	int		i;
+
+	i = 0;
+	if (s == NULL || f == NULL)
+		return (NULL);
+	newstr = (char *)malloc(ft_strlen(s) * sizeof(char));
+	if (newstr == NULL)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		newstr[i] = f(i, s[i]);
+		i++;
+	}
+	newstr[i] = '\0';
+	return (newstr);
 }
 
-char *ft_strmapi(char const *s, char(*f)(unsigned int, char))
-{
-    char *newstr;
-    int i;
-    i = 0;
-    
-    if( s == NULL || f == NULL)
-        return (NULL);
-    newstr = (char *)malloc(ft_strlen(s) * sizeof(char));
-    if( newstr == NULL)
-        return NULL;
-    while(s[i] != '\0')
-    {
-        newstr[i] = f(i, s[i]);
-        i++;
-    }
-    newstr[i] = '\0';
-    return (newstr);
-}
-
-char tester(unsigned int i, char c) // necessary to create a function to the pointer points
-{   
-    return (i + c);
-}
-   
-int main()
-{
-    char *f = ft_strmapi("000", tester); // output 48, 49, 50
-    printf("%s", f);
-}
+// char tester(unsigned int i, char c)
+// {   
+//     return (i + c);
+// }
+// int main()
+// {
+//     char *f = ft_strmapi("000", tester); // output 48, 49, 50
+//     printf("%s", f);
+// }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,43 +6,38 @@
 /*   By: edribeir <edribeir@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/04 14:43:39 by edribeir      #+#    #+#                 */
-/*   Updated: 2023/10/09 18:07:06 by edribeir      ########   odam.nl         */
+/*   Updated: 2023/10/25 14:25:08 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
+#include "libft.h"
 
-int ft_strncmp(char *str, char *str1, unsigned int n)
+int	ft_strncmp(char *str, char *str1, size_t n)
 {
-    int i;
-    int j;
-    i = 0;
-    j = 0;
-    if (n == 0)
-        return (0);
-    while (n > 0)
-    {
-        if (str[i] != str1[j])
-        {
-            return (str[i] - str1[j]);
-            i++;
-            j++;
-        }
-        n--;
-    }
-    return (0);
+	size_t	i;
+
+	i = 0;
+	if (n == 0)
+		return (0);
+	while (n > 0)
+	{
+		if (str[i] != str1[i])
+		{
+			return (str[i] - str1[i]);
+			i++;
+		}
+		n--;
+	}
+	return (0);
 }
 
-int main()
-{
-    char a[] = "world";
-    char b[] = "hello";
-    // printf("first string: %s, second string %s\n result %d","Maria","Joao", strncmp("Maria", "Joao",1));
-    char result = strncmp(a, b, 0);
-    
-    printf("%d", result);
-    char result2 = ft_strncmp(a, b, 0);
-    printf("\n%d", result2);
-    return (0);
-}
+// int main()
+// {
+//     char a[] = "world";
+//     char b[] = "hello";
+//     char result = strncmp(a, b, 0);
+//     printf("%d", result);
+//     char result2 = ft_strncmp(a, b, 0);
+//     printf("\n%d", result2);
+//     return (0);
+// }
