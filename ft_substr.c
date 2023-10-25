3f5f96ff03658592070282e907dda237f35e57b2
dudamakesthings <dudamakesthings@gmail.com>
2023-10-25 18:48:40 +0200

    add the pointer that i erased before

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: edribeir <edribeir@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/16 09:04:32 by edribeir      #+#    #+#                 */
-/*   Updated: 2023/10/25 09:47:26 by edribeir      ########   odam.nl         */
+/*   Updated: 2023/10/25 17:18:49 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	src_len;
-	char	substr;
+	char	*substr;
 	int		i;
 
 	i = 0;
@@ -28,16 +28,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		substr = (char *)malloc((len + 1) * sizeof(char));
 	else
 		substr = (char *)malloc((src_len - start + 1) * sizeof(char));
-	if (substr == 0)
-		return (0);
+	if (substr == NULL)
+		return (NULL);
 	ft_strlcpy (substr, (&s[start]), len + 1);
 	return (substr);
 }
 
 // int main()
 // {
-//     char str[] = "A";
-//     // char str[] = "Codam is a really nice place";
+//     // char str[] = "A";
+//     char str[] = "Codam is a really nice place";
 //     char *m = ft_substr(str, 3, 40);
 //     printf("%s\n", m);
 // }
