3ab2d4e190422d25169223658e518877b45a55b9
dudamakesthings <dudamakesthings@gmail.com>
2023-10-25 18:47:49 +0200

    create new static function for the norminette pass

--- ft_split.c
+++ ft_split.c
@@ -6,143 +6,110 @@
 /*   By: edribeir <edribeir@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/17 10:42:31 by edribeir      #+#    #+#                 */
-/*   Updated: 2023/10/18 16:04:27 by edribeir      ########   odam.nl         */
+/*   Updated: 2023/10/25 18:46:47 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-unsigned int ft_strlcpy(char *dst, const char *src, unsigned int size)
+
+static char	**ft_free(char **array_str)
 {
-    unsigned int i;
-    i = 0;
-    while(i < (size - 1))
-    {
-        dst[i] = src[i];
-        i++;
-    }
-    dst[i] = 0;
-    return i;
+	int	word;
+
+	word = 0;
+	while (array_str[word])
+	{
+		free(array_str[word]);
+		word++;
+	}
+	free(array_str);
+	return (NULL);
 }
 
-unsigned int ft_strlen (const char *str)
-{   
-    unsigned int i;
-    i = 0;
-    while(str[i] != '\0')
-        i++;
-    return i;
-}
-
-char *ft_substr(char const *s, unsigned int start, size_t len)
+static int	ft_get_words(char const *s, char c)
 {
-    size_t src_len;
-    char *substr;
-    int i;
-    i = 0;
-    
-    if(s[i] == '\0')
-        return (NULL);
-    src_len = ft_strlen(s);
-    if(start > src_len)
-        return (NULL);
-    if(src_len - start >= len)
-        substr = (char*)malloc((len + 1) * sizeof(char));
-    else
-        substr = (char *)malloc((src_len - start + 1) * sizeof(char));
-    if(substr == NULL)
-        return (NULL);
-    ft_strlcpy(substr,(&s[start]), len + 1);
-    return (substr);
+	unsigned int	words;
+	int				i;
+
+	i = 0;
+	words = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			i++;
+		else
+		{
+			words++;
+			while (s[i] != '\0' && s[i] != c)
+				i++;
+		}
+	}
+	return (words);
 }
 
-static char **ft_free(char **array_str)
+static char	**create_array(char **array_str, char const *s, char c)
 {
-    int word;
-    word = 0;
-    while(array_str[word]) //when it is a string different than null, free every word first
-    {
-        free(array_str[word]);
-        word++;
-    }
-    free(array_str); // free the space of all the substr
-    return (NULL);
+	int		wordstart;
+	size_t	word_len;
+	int		i;
+	int		word;
+
+	word = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			i++;
+		else
+		{
+			wordstart = i;
+			while (s[i] != '\0' && s[i] != c)
+				i++;
+			word_len = (size_t)(i - wordstart);
+			array_str[word] = ft_substr(s, wordstart, word_len);
+			if (array_str[word] == NULL)
+				return (ft_free(array_str));
+			word++;
+		}
+	}
+	array_str[word] = NULL;
+	return (array_str);
 }
 
-//The ft_split function splits a string into an array of substrings based on a delimiter character.
-static int ft_get_words(char const *s, char c) // static because it is just used in this function
+char	**ft_split(char const *s, char c)
 {
-    unsigned int words;
-    int i;
-    i = 0;
-    words = 0;
+	char	**array_str;
+	int		i;
 
-    while(s[i] != '\0')
-    {
-        if(s[i] == c)
-            i++;
-        else
-        {
-            words++;
-            while(s[i] != '\0' && s[i] != c)
-                i++;
-        } 
-    }
-    return(words);
-}
-// ft_get_words count how many "words" existe in the string when the separator works
-char **ft_split(char const *s, char c)
-{
-    char **array_str;
-    int wordstart;
-    size_t word_len;
-    int i;
-    int word;
-    word = 0;
-    i = 0;
-
-    if(s[i] == '\0')
-        return (NULL);
-    array_str = (char **)malloc((ft_get_words(s,c) + 1) * sizeof(char *));
-    if(array_str == NULL)
-        return NULL;
-    while(s[i] != '\0')
-    {
-        if(s[i] == c)
-            i++;
-        else
-        {
-            wordstart = i;
-            while(s[i] != '\0' && s[i] != c)
-                i++;
-            word_len = (size_t)(i - wordstart);
-            array_str[word] = ft_substr(s, wordstart, word_len);
-            if(array_str[word] == NULL)
-                return(ft_free(array_str));
-            word++;
-        }
-    }
-    array_str[word] = NULL;
-    return (array_str);
+	i = 0;
+	if (s[i] == '\0')
+		return (NULL);
+	array_str = (char **)malloc((ft_get_words(s, c) + 1) * sizeof(char *));
+	if (array_str == NULL)
+		return (NULL);
+	array_str = create_array(array_str, s, c);
+	if (array_str == NULL)
+		return (NULL);
+	return (array_str);
 }
 
-int main()
-{
-    int i;
-    // char str[] = "";
-    char str[] = "Hello Human Coder";
-    char c = ' ';
+// int main()
+// {
+//     int i;
+//     // char str[] = "";
+//     char str[] = "Hello Human Coder";
+//     char c = ' ';
 
-    i = 0;
-    // printf("%d\n", ft_get_words(str, c));
-    char **m = ft_split(str, c);
-    if (ft_split(str, c) == NULL) //test when it is a null 
-    {
-        printf("%s\n", "Null");
-    }
-    while(m[i])
-    {
-        printf("%s\n", m[i]);
-        i++;
-    }
-    
-}
+//     i = 0;
+//     // printf("%d\n", ft_get_words(str, c));
+//     char **m = ft_split(str, c);
+//     // if (ft_split(str, c) == NULL) //test when it is a null 
+//     // {
+//     //     printf("%s\n", "Null");
+//     // }
+//     while(m[i])
+//     {
+//         printf("%s\n", m[i]);
+//         i++;
+//     }
+// }
