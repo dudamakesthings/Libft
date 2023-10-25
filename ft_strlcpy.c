99bedd1937e7bf94c25245b7ae73f0195afbcd1e
dudamakesthings <dudamakesthings@gmail.com>
2023-10-25 14:06:24 +0200

    change to size_t

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,29 +6,29 @@
 /*   By: edribeir <edribeir@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/06 10:00:18 by edribeir      #+#    #+#                 */
-/*   Updated: 2023/10/06 16:39:36 by edribeir      ########   odam.nl         */
+/*   Updated: 2023/10/25 14:03:49 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <bsd/string.h>
+#include "libft.h"
 
-unsigned int ft_strlcpy(char *dst, const char *src, unsigned int size)
+size_t	ft_strlcpy(char *dst, const char *src, size_t destsize)
 {
-    int i;
-    i = 0;
-    while(i < (size - 1))
-    {
-        dst[i] = src[i];
-        i++;
-    }
-    dst[i] = 0;
-    return i;
+	size_t	i;
+
+	i = 0;
+	while (i < (destsize - 1))
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (i);
 }
-int main()
-{
-    char source[] = "hello";
-    char dest[6] = "Maria";
-    ft_strlcpy(dest, source, 6); //the last number should be the size of destination.
-    printf("%s", dest);
-}
+// int main()
+// {
+//     char source[] = "hello";
+//     char dest[6] = "Maria";
+//     ft_strlcpy(dest, source, 6);
+//     printf("%s", dest);
+// }
