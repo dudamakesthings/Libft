026bd5d3a03f83b6f716b4dbb74603d47915ff3f
dudamakesthings <dudamakesthings@gmail.com>
2023-10-25 18:51:03 +0200

    add norminette ajusts

--- ft_strchr.c
+++ ft_strchr.c
@@ -1,36 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: edribeir <edribeir@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/03 16:45:20 by edribeir          #+#    #+#             */
-/*   Updated: 2023/10/04 18:35:30 by edribeir         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   ft_strchr.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: edribeir <edribeir@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/10/03 16:45:20 by edribeir      #+#    #+#                 */
+/*   Updated: 2023/10/25 15:46:29 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
+#include "libft.h"
 
-char *ft_strchr(const char *str, int c)
+char	*ft_strchr(const char *str, int c)
 {
-    int i;
-    i = 0;
-    
-    while(str[i] != '\0')
-    {
-        if(str[i] == c)
-            return (char *)&str[i]; // works with (char *)str;
-        i++;
-    }
-    return NULL;
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] == c)
+			return ((char *)&str[i]);
+		i++;
+	}
+	return (NULL);
 }
 
-int main()
-{
-    char str[] = "ana";
-    int ch = 'n';
-    
-    printf("new frase: %s", ft_strchr(str, ch));
-}
+// int main()
+// {
+//     char str[] = "ana";
+//     int ch = 'n';
+//     printf("new frase: %s", ft_strchr(str, ch));
+// }
+
+//(char *)&str[i] works with (char *)str;
