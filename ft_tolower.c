fe5cf1fc654351aa113feef2b6a2ef1d30c92b92
dudamakesthings <dudamakesthings@gmail.com>
2023-10-25 12:26:29 +0200

    Make norminette ajusts

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,38 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_tolower.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: edribeir <edribeir@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/03 16:36:38 by edribeir          #+#    #+#             */
-/*   Updated: 2023/10/03 16:43:31 by edribeir         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   ft_tolower.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: edribeir <edribeir@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/10/03 16:36:38 by edribeir      #+#    #+#                 */
+/*   Updated: 2023/10/25 09:42:16 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <ctype.h>
-#include <stdio.h>
+#include "libft.h"
 
-char ft_tolower(char c)
+int	ft_tolower(int c)
 {
-    if(c >= 'A' && c <= 'Z')
-        c = c + 32;
-    return c;
+	if (c >= 'A' && c <= 'Z')
+		c = c + 32;
+	return (c);
 }
 
-int main()
-{
-    char c;
-
-    c = 'a';
-    printf("%c -> %c", c, ft_tolower(c));
-    
-    c = 'B';
-    printf("\n%c -> %c", c, ft_tolower(c));
-    
-    c = 'E';
-    printf("\n%c -> %c", c, ft_tolower(c));
-
-    c = '9';
-    printf("\n%c -> %c", c, ft_tolower(c));
-}
+// int main()
+// {
+//     int c;
+//     c = 'a';
+//     printf("%c -> %c", c, ft_tolower(c));
+//     c = 'B';
+//     printf("\n%c -> %c", c, ft_tolower(c));
+//     c = 'E';
+//     printf("\n%c -> %c", c, ft_tolower(c));
+//     c = '9';
+//     printf("\n%c -> %c", c, ft_tolower(c));
+// }
--- ft_toupper.c
+++ ft_toupper.c
@@ -1,39 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_toupper.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: edribeir <edribeir@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/03 16:12:55 by edribeir          #+#    #+#             */
-/*   Updated: 2023/10/03 16:35:50 by edribeir         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   ft_toupper.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: edribeir <edribeir@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/10/03 16:12:55 by edribeir      #+#    #+#                 */
+/*   Updated: 2023/10/25 09:42:32 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <ctype.h>
+#include "libft.h"
 
-char ft_toupper(char c)
+int	ft_toupper(int c)
 {
-    if (c >= 'a' && c <= 'z')
-        c = c - 32;
-    return c;
-
+	if (c >= 'a' && c <= 'z')
+		c = c - 32;
+	return (c);
 }
-
-int main() 
-{
-    char c;
-
-    c = 'm';
-    printf("%c -> %c", c, ft_toupper(c));
-
-    c = 'e';
-    printf("\n%c -> %c", c, ft_toupper(c));
-
-    c = 'E';
-    printf("\n%c -> %c", c, ft_toupper(c));
-
-    c = '0';
-    printf("\n%c -> %c", c, ft_toupper(c));
-}
+// int main() 
+// {
+//     int c;
+//     c = 'm';
+//     printf("%c -> %c", c, ft_toupper(c));
+//     c = 'e';
+//     printf("\n%c -> %c", c, ft_toupper(c));
+//     c = 'E';
+//     printf("\n%c -> %c", c, ft_toupper(c));
+//     c = '0';
+//     printf("\n%c -> %c", c, ft_toupper(c));
+// }
