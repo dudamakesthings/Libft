20eb27a42c57220823a0d3f2a22d1f5b629e7ef1
dudamakesthings <dudamakesthings@gmail.com>
2023-10-25 14:27:07 +0200

    change to size_t

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,39 +6,36 @@
 /*   By: edribeir <edribeir@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/06 16:44:12 by edribeir      #+#    #+#                 */
-/*   Updated: 2023/10/09 11:10:13 by edribeir      ########   odam.nl         */
+/*   Updated: 2023/10/25 14:26:00 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <bsd/string.h> // functions from BSD systems -lbsd
-#include <stdio.h>
+#include "libft.h"
 
-unsigned int ft_strlcat(char *dst, const char *src, unsigned int dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-    int i;
-    int i2;
-    i= 0;
-    i2 = 0;
-    while(dst[i] != '\0')
-         i++;
-    while((src[i2] != '\0') && (i < dstsize - 1))
-    {
-        dst[i] = src[i2];
-        i++;
-        i2++;
-    }
-     dst[i] = '\0';
-     return i;
+	size_t	i;
+	size_t	j;
+
+	i = 0;
+	j = 0;
+	ft_strlen(dst);
+	while ((src[j] != '\0') && (i < dstsize - 1))
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (i);
 }
 
-int main()
-{
-    char source[] = "Catnip";
-    char dest[] = "Whiskas Sache";
-    
-    ft_strlcat(dest, source, 5);
-    printf("%s", dest);
-    strlcat(dest, source, 5);
-    printf("\n%s", dest);
-    
-}
+// int main()
+// {
+//     char source[] = "Catnip";
+//     char dest[] = "Whiskas Sache";
+//     ft_strlcat(dest, source, 30);
+//     printf("%s", dest);
+//     strlcat(dest, source, 5); // compile with -lbsd
+//     printf("\n%s", dest);
+// }
