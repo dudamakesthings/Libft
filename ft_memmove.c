243833f3b3c78aa14f081425429b3b7ddd0cec25
dudamakesthings <dudamakesthings@gmail.com>
2023-10-26 10:14:28 +0200

    new function with details

--- ft_memmove.c
+++ ft_memmove.c
@@ -1,48 +1,57 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memmove.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: edribeir <edribeir@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/06 09:08:33 by edribeir          #+#    #+#             */
-/*   Updated: 2023/10/06 09:56:39 by edribeir         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   ft_memmove.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: edribeir <edribeir@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/10/06 09:08:33 by edribeir      #+#    #+#                 */
+/*   Updated: 2023/10/26 10:12:57 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
+#include "libft.h"
 
-void *ft_memmove(void *dest, const void *src, unsigned int n)
+void	*ft_memmove(void *dst, const void *src, size_t nbytes)
 {
-    //first copied into a temporary array that does not overlap src or dest, and the bytes are then copied from the temporary array to dest.
-    char *pdest = dest;
-    const char *psrc = src;
-    char temp[n]; // allocate memory for the temp array not supost to be an pointer
-    int i;
-    i = 0;
-    while(i < n)
-    {
-        temp[i] = psrc[i];
-        i++;
-    }
-    i = 0;
-    while(i < n)
-    {
-        pdest[i] = temp[i];
-        i++;
-    }
-    return (dest);
+	size_t	i;
+	char	*pdst;
+	char	*psrc;
+
+	pdst = (char *)dst;
+	psrc = (char *)src;
+	i = 0;
+	if ((dst == NULL) && (src == NULL))
+		return (NULL);
+	if (dst <= src)
+	{
+		while (nbytes > i)
+		{
+			pdst[i] = psrc[i];
+			i++;
+		}
+	}
+	else if (dst > src)
+	{
+		while (nbytes > 0)
+		{
+			pdst[nbytes - 1] = psrc[nbytes - 1];
+			nbytes--;
+		}
+	}
+	return (dst);
 }
 
 int main()
 {
-    char source[] = {1, 2, 3, 4, 6};
-    char destin[8];
-    
-   ft_memmove(destin, source, 3);
+    // char source[] = {1, 2, 3, 4, 6};
+    // char destin[8];
+	char source[] = {1, 2, 3, 4, 6};
+    char destin[] = {0, 0, 0};
+	
+   char *m = ft_memmove(destin, source, 3);
       
     int j = 0;
     while (destin[j])
-        printf("%d", destin[j++]);
+        printf("%d", m[j++]);
 }
