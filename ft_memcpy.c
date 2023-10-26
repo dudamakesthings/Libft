dbc12c20de969c398c4e5addf02422d75c1a5ff4
dudamakesthings <dudamakesthings@gmail.com>
2023-10-26 11:27:19 +0200

    Use casting and norminette ajust

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,43 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_memcpy.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: edribeir <edribeir@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/05 14:12:48 by edribeir          #+#    #+#             */
-/*   Updated: 2023/10/06 09:34:50 by edribeir         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   ft_memcpy.c                                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: edribeir <edribeir@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/10/05 14:12:48 by edribeir      #+#    #+#                 */
+/*   Updated: 2023/10/26 11:25:14 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <string.h>
-#include <stdio.h>
+#include "libft.h"
 
-void *ft_memcpy(void *dest, const void *src, unsigned int n)
+void	*ft_memcpy(void *dst, const void *src, size_t nbytes)
 {
-    char *pdest = dest;
-    const char *psrc = src;
-    int i;
-    i = 0;
-    while(i < n)
-    {    
-        pdest[i] = psrc[i];
-        i++;
-    }
-    return (dest); 
+	size_t	i;
+
+	i = 0;
+	while (i < nbytes)
+	{
+		((char *)dst)[i] = ((char *)src)[i];
+		i++;
+	}
+	return (dst);
 }
 
-int main()
-{
-    char source[] = {1, 2, 3, 4, 6};
-    char destin[8];
-    
-   ft_memcpy(destin, source, 3);
-    // printf("%s\n", destin);
-    // ft_memcpy(destin, source, 3);
-    // printf("%s\n", destin);
-    
-    int j = 0;
-    while (destin[j])
-        printf("%d", destin[j++]);
-}
+// int main()
+// {
+//     char source[] = {1, 2, 3, 4, 6};
+//     char destin[8];
+//    ft_memcpy(destin, source, 3);
+//     // printf("%s\n", destin);
+//     // ft_memcpy(destin, source, 3);
+//     // printf("%s\n", destin);
+//     int j = 0;
+//     while (destin[j])
+//         printf("%d", destin[j++]);
+// }
