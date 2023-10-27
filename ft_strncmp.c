85bc542b348660739efe2543137c86defc245bbe
dudamakesthings <dudamakesthings@gmail.com>
2023-10-27 16:48:23 +0200

    Add somo conditions on the while

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,27 +6,22 @@
 /*   By: edribeir <edribeir@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/04 14:43:39 by edribeir      #+#    #+#                 */
-/*   Updated: 2023/10/25 14:25:08 by edribeir      ########   odam.nl         */
+/*   Updated: 2023/10/27 16:44:35 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_strncmp(char *str, char *str1, size_t n)
+int	ft_strncmp(const char *str1, const char *str2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
-	if (n == 0)
-		return (0);
-	while (n > 0)
+	while (i < n && (str1[i] != '\0' || str2[i] != '\0'))
 	{
-		if (str[i] != str1[i])
-		{
-			return (str[i] - str1[i]);
-			i++;
-		}
-		n--;
+		if (str1[i] != str2[i])
+			return ((unsigned char)str1[i] - (unsigned char)str2[i]);
+		i++;
 	}
 	return (0);
 }
@@ -35,9 +30,9 @@ int	ft_strncmp(char *str, char *str1, size_t n)
 // {
 //     char a[] = "world";
 //     char b[] = "hello";
-//     char result = strncmp(a, b, 0);
-//     printf("%d", result);
-//     char result2 = ft_strncmp(a, b, 0);
+//     char result = strncmp(a, b, 2);
+//     printf("%d original function", result);
+//     char result2 = ft_strncmp(a, b, 2);
 //     printf("\n%d", result2);
 //     return (0);
 // }
