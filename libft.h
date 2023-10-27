9a00642ecca04b91b4f4b1d41345f7c617dc6bd9
dudamakesthings <dudamakesthings@gmail.com>
2023-10-27 11:46:24 +0200

    add all obrigatory functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: edribeir <edribeir@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/24 12:31:26 by edribeir      #+#    #+#                 */
-/*   Updated: 2023/10/26 19:38:13 by edribeir      ########   odam.nl         */
+/*   Updated: 2023/10/27 11:40:12 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,11 @@
 // Character Function
 int		ft_toupper(int c);
 int		ft_tolower(int c);
+int		ft_isprint(int c);
+int		ft_isdigit(int c);
+int		ft_isascii(int c);
+int		ft_isalpha(int c);
+int		ft_isalnum(int c);
 
 // // String functions
 char	*ft_substr(char const *s, unsigned int start, size_t len);
@@ -42,6 +47,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_strdup(const char *str);
 char	*ft_strchr(const char *str, int c);
 char	**ft_split(char const *s, char c);
+void	ft_bzero(void *s, size_t n);
 
 // Write Functions
 void	ft_putstr_fd(char *s, int fd);
@@ -55,6 +61,10 @@ void	*ft_memmove(void *dst, const void *src, size_t nbytes);
 void	*ft_memcpy(void *dst, const void *src, size_t nbytes);
 int		ft_memcmp(const void *str1, const void *str2, size_t nbytes);
 void	*ft_memchr(const void *s, int c, size_t nbytes);
+void	*ft_calloc(size_t nitems, size_t sizeitem);
 
+// Math functions
+char	*ft_itoa(int n);
+int		ft_atoi(const char *str);
 
 #endif
