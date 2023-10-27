5322ae0cccd68c9a465cf423c36ede6b66eb6b9e
dudamakesthings <dudamakesthings@gmail.com>
2023-10-27 18:38:00 +0200

    New changes

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: edribeir <edribeir@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/24 12:31:26 by edribeir      #+#    #+#                 */
-/*   Updated: 2023/10/27 11:40:12 by edribeir      ########   odam.nl         */
+/*   Updated: 2023/10/27 16:47:52 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,10 +37,10 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strrchr(const char *str, int ch);
 char	*ft_strnstr(const char *big, const char *little, size_t len);
-int		ft_strncmp(const char *str, const char *str1, size_t n);
+int		ft_strncmp(const char *str1, const char *str2, size_t n);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 size_t	ft_strlen(const char *str);
-size_t	ft_strlcpy(char *dst, const char *src, size_t size);
+size_t	ft_strlcpy(char *dst, const char *src, size_t destsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 char	*ft_strjoin(char const *s1, char const *s2);
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
