2741537f046ba6b0002bd93dd9e6659e1ef69064
dudamakesthings <dudamakesthings@gmail.com>
2023-10-27 12:15:19 +0200

    fix a problem

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: edribeir <edribeir@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/19 16:28:14 by edribeir      #+#    #+#                 */
-/*   Updated: 2023/10/26 18:56:13 by edribeir      ########   odam.nl         */
+/*   Updated: 2023/10/27 12:08:56 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 		return ;
 	while (s[i] != '\0')
 	{
-		f(i, s);
+		f(i, (s + i));
 		i++;
 	}
 }
 
 // void tester(unsigned int index, char *c)
 // {
-// 	c[index] = ft_toupper(c[index]);
+// 	*c = ft_toupper(*c);
 // }
 
 // int main()
