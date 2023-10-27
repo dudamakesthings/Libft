d9f0e312d319ecee9ec0434b2f18ee9566e5d9a0
dudamakesthings <dudamakesthings@gmail.com>
2023-10-27 12:20:59 +0200

    exclude the 127 character (DEL)

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,7 +6,7 @@
 /*   By: edribeir <edribeir@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/03 13:58:57 by edribeir      #+#    #+#                 */
-/*   Updated: 2023/10/27 11:03:37 by edribeir      ########   odam.nl         */
+/*   Updated: 2023/10/27 12:19:57 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_isprint(int c)
 {
-	if (c >= 32 && c <= 127)
+	if (c >= 32 && c <= 126)
 		return (1);
 	else
 		return (0);
