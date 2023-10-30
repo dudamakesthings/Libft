d14a14a7ba3f11695a8eed0361963c924ba26e87
dudamakesthings <dudamakesthings@gmail.com>
2023-10-30 11:23:12 +0100

    add while condition

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: edribeir <edribeir@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/11 11:23:36 by edribeir      #+#    #+#                 */
-/*   Updated: 2023/10/27 11:39:51 by edribeir      ########   odam.nl         */
+/*   Updated: 2023/10/30 11:19:06 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,19 +21,18 @@ int	ft_atoi(const char *str)
 	i = 0;
 	sign = 1;
 	result = 0;
-	while (str[i] != '\0')
+	while (str[i] == ' ' || str[i] == '\t' || str[i] == '\n'
+		|| str[i] == '\v' || str[i] == '\f' || str[i] == '\r')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
 	{
-		if (str[i] == '-' || str[i] == '+')
-		{
-			if (str[i] == '-')
-				sign = sign * -1;
-		}
-		if (str[i] == ' ')
-			i++;
-		if ((str[i] >= '0') && (str[i] <= '9'))
-		{
-			result = (result * 10) + (str[i] - 48);
-		}
+		if (str[i] == '-')
+			sign = sign * -1;
+		i++;
+	}
+	while ((str[i] >= '0') && (str[i] <= '9'))
+	{
+		result = (result * 10) + (str[i] - 48);
 		i++;
 	}
 	return (result * sign);
@@ -44,9 +43,9 @@ int	ft_atoi(const char *str)
 //     int val;
 //     int val2;
 //     // char str[20] = "Edddd";
-//     char str[20] = "120";
+//     char str[20] = "\007 8";
 //     val = ft_atoi(str);
 //     printf("%d", val);
-//     val2 = ft_atoi(str);
+//     val2 = atoi(str);
 //     printf("\n%d", val2);
 // }
