23e6d32b528e318ff60d7ed201aa569345efd33e
dudamakesthings <dudamakesthings@gmail.com>
2023-10-30 17:19:42 +0100

    fix my correction for Null string

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: edribeir <edribeir@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/13 10:37:43 by edribeir      #+#    #+#                 */
-/*   Updated: 2023/10/25 15:42:02 by edribeir      ########   odam.nl         */
+/*   Updated: 2023/10/30 17:19:12 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,6 @@ char	*ft_strdup(const char *str)
 	int		len;
 
 	i = 0;
-	if (str == NULL)
-		return (NULL);
 	len = ft_strlen(str) + 1;
 	dup = (char *)malloc(len * sizeof(char));
 	if (dup == NULL)
