36009a0b0456888bd421ceed4975bbc071dba204
dudamakesthings <dudamakesthings@gmail.com>
2023-10-30 17:03:12 +0100

    Change a condition for dstsize 0

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: edribeir <edribeir@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/06 10:00:18 by edribeir      #+#    #+#                 */
-/*   Updated: 2023/10/27 12:32:52 by edribeir      ########   odam.nl         */
+/*   Updated: 2023/10/30 16:46:05 by edribeir      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,18 @@
 size_t	ft_strlcpy(char *dst, const char *src, size_t destsize)
 {
 	size_t	i;
+	char	*p_src;
 
 	i = 0;
-	if (destsize > 0)
+	p_src = (char *)src;
+	if (destsize == 0)
+		return (ft_strlen(src));
+	while (i < (destsize - 1) && p_src[i] != '\0')
 	{
-		while (i < (destsize - 1) && src[i] != '\0')
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
+		dst[i] = p_src[i];
+		i++;
 	}
+	dst[i] = '\0';
 	return (ft_strlen(src));
 }
 
@@ -33,6 +34,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t destsize)
 // {
 //     char source[] = "hello";
 //     char dest[6] = "Maria";
-//     int f=ft_strlcpy(dest, source, 6);
+//     int f=ft_strlcpy(dest, source, 0);
 //     printf("%d", f);
 // }
