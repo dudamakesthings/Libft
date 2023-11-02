dfaab0edb777fa33e3e13fdec6890075a8c23d04
dudamakesthings <dudamakesthings@gmail.com>
2023-11-02 19:01:55 +0100

    Add bonus part

--- libft.h
+++ libft.h
@@ -26,11 +26,23 @@
 // Bonus Struct
 typedef struct	s_list
 {
-	void	*content;
-	struct	s_list *next;
-} t_list;
+	void			*content;
+	struct	s_list 	*next;
+} 			t_list;
 
-// Character Function
+// Bonus Functions
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst_head, t_list *new_node);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst_head, t_list *new_node);
+void	ft_lstdelone(t_list *lst_node, void (*del)(void *));
+void	ft_lstclear(t_list **lst, void (*del)(void *));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+
+
+// Character Functions
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_isprint(int c);
